20921e904d62a411d297f9bbf649ce7a967b4d29
yusun-lab <yu.sun@hyperisland.se>
2024-12-30 19:06:56 +0100

    finish the click to draw

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let color = "black";
+let click = false;
+
 document.addEventListener('DOMContentLoaded', function() {
   createBoard(16);
+
+  document.querySelector("body").addEventListener("click", function(e) {
+    if(e.target.tagName != "BUTTON") {
+        click = !click;
+        let draw = document.querySelector("#draw");
+        if(click) {
+            draw.innerHTML = "Now You Can Draw!";
+        }
+        else {
+            draw.innerHTML = "You are Not Allowed To Draw!";
+        }
+    }
+  })
 
   let btn_popup = document.querySelector('#popup');
   btn_popup.addEventListener("click", function() {
@@ -40,11 +55,13 @@ function getSize() {
 }
 
 function colorDiv() {
-    if(color == "random") {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    }
-    else{
-        this.style.backgroundColor = 'black'
+    if(click) {
+        if(color == "random") {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else{
+            this.style.backgroundColor = 'black'
+        }
     }
 }
 
